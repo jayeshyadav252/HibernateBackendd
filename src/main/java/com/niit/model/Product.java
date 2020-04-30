084ec79b10e10066bbfeddb8d6927fe00bc2e9c2
jayeshyadav252 <jayeshyadav252@gmail.com>
2020-04-30 16:19:35 +0530

    changes

---
+++ src/main/java/com/niit/model/Product.java
@@ -0,0 +1,47 @@
+package com.niit.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
+public class Product
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int productid;
+	
+	@Column
+	String productname;
+	Double productprice;
+	int quantity;
+	public int getProductid() {
+		return productid;
+	}
+	public void setProductid(int productid) {
+		this.productid = productid;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public Double getProductprice() {
+		return productprice;
+	}
+	public void setProductprice(Double productprice) {
+		this.productprice = productprice;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+}
